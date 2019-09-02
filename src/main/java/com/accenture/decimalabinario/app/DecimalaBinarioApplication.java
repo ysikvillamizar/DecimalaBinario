998c93632fa88eba507437e82cc43046082dfc53
ysikvillamizar <ysikvillamizar@gmail.com>
2019-09-01 21:44:45 -0500

    programa

--- src/main/java/com/accenture/decimalabinario/app/DecimalaBinarioApplication.java
+++ src/main/java/com/accenture/decimalabinario/app/DecimalaBinarioApplication.java
@@ -1,4 +1,5 @@
 package com.accenture.decimalabinario.app;
+import java.util.Scanner;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -8,6 +9,20 @@ public class DecimalaBinarioApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DecimalaBinarioApplication.class, args);
+		 Scanner scanner= new Scanner (System.in);
+	        
+	       System.out.println("Ingrese numero: ");
+	        int decimal=scanner.nextInt();
+	        int cociente=decimal;
+	        int digito;
+	        String binario="";
+	        while (cociente>1){
+	            digito=cociente%2;
+	            cociente=cociente/2;
+	            binario=digito+binario;
+	        }
+	        binario=cociente+binario;
+	        System.out.println("El numero " +decimal+ " en binario es: " +binario );
 	}
 
 }
