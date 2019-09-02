8ec3af095c3fcf87c3bd2ab31261a1262a373086
ysikvillamizar <ysikvillamizar@gmail.com>
2019-09-01 21:09:56 -0500

    mi decimal binario

---
+++ src/main/java/com/accenture/decimalabinario/app/DecimalaBinarioApplication.java
@@ -0,0 +1,13 @@
+package com.accenture.decimalabinario.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DecimalaBinarioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DecimalaBinarioApplication.class, args);
+	}
+
+}
